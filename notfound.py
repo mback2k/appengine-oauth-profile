481028f075bf46696b8adc5904663e97bc883c52
Marc Hoersken <info@marc-hoersken.de>
2012-07-02 10:42:31 +0200

    Return HTTP Status Code 404 for not found errors

--- notfound.py
+++ notfound.py
@@ -4,6 +4,8 @@ import os
 
 class NotFound(webapp2.RequestHandler):
     def get(self):
+        self.error(404)
+
         path = os.path.join(os.path.dirname(__file__), 'templates/notfound.html')
         self.response.out.write(template.render(path, {}))
 
