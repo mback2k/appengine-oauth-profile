95ab496b66d2e010589666833ac789ddf174c9a8
Marc Hoersken <info@marc-hoersken.de>
2012-07-02 15:12:50 +0200

    Added robots.txt

---
+++ robots.py
@@ -0,0 +1,12 @@
+from google.appengine.ext.webapp import template
+import webapp2
+import os
+
+class Robots(webapp2.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+
+        path = os.path.join(os.path.dirname(__file__), 'templates/robots.txt')
+        self.response.out.write(template.render(path, {}))
+
+app = webapp2.WSGIApplication([('/robots.txt', Robots)])
